d446be59b40987a3ab8f447676aec46ba2f6b6a6
psuman <sumanpapanaboina1983@gmail.com>
2019-02-28 08:58:13 +0530

    added redis and mongo dependencies

--- service/cache/cachefinder.go
+++ service/cache/cachefinder.go
@@ -1,6 +1,9 @@
 package cache
 
-import common "github.com/psuman/go-training/service/common"
+import (
+	"github.com/go-redis/redis"
+	common "github.com/psuman/go-training/service/common"
+)
 
 //CacheFinder retrieves item with productId from cache
 type CacheFinder interface {
@@ -9,6 +12,22 @@ type CacheFinder interface {
 
 //RedisCacheFinder retrieves item with productId from redis cache
 type RedisCacheFinder struct{}
+
+//NewClient creates new redis client
+func (RedisCacheFinder) NewClient(connUrl string) (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     connUrl,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err := client.Ping().Result()
+
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
 
 //FindItemInCache retrieves item from redis cache
 func (RedisCacheFinder) FindItemInCache(ProductID string) (common.ProductDetails, error) {
--- service/persistence/itemdao.go
+++ service/persistence/itemdao.go
@@ -1,6 +1,13 @@
 package persistence
 
-import common "github.com/psuman/go-training/service/common"
+import (
+	"context"
+	"time"
+
+	"github.com/mongodb/mongo-go-driver/mongo/readpref"
+	common "github.com/psuman/go-training/service/common"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 //ItemDao retrieves item with productId from database
 type ItemDao interface {
@@ -9,6 +16,19 @@ type ItemDao interface {
 
 //MongoItemDao retrieves item with productId from mongo database
 type MongoItemDao struct{}
+
+//NewClient creates and returns connection to MongoDb
+func (MongoItemDao) NewClient(connUri string) (*mongo.Client, error) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, connUri)
+	err = client.Ping(ctx, readpref.Primary())
+
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+
+}
 
 //FindItem retrieves item from Mongo database
 func (MongoItemDao) FindItem(ProductID string) (common.ProductDetails, error) {
